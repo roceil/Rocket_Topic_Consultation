64c922cff54ace94555692a7077fa0c3dc1db859
roceil <a0978006326@gmail.com>
2023-03-07 19:01:38 +0800

    [Feat] finish platformFeature layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-
 import {
   SearchOutlined,
   ShoppingCartOutlined,
@@ -127,8 +125,8 @@ export default function Home() {
         {/* 推薦諮商師 */}
         <section className='bg-[#FAFAFF] '>
           <div className='container flex w-screen flex-col items-center  py-20 text-center'>
-            <h2 className='font-bold text-primary-heavy '>推薦諮商師</h2>
-            <p className='mt-2 mb-3 text-primary lg:mt-3 lg:mb-[76px] lg:max-w-[612px] lg:text-lg'>
+            <h2>推薦諮商師</h2>
+            <p className='subTitle'>
               Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit
               phasellus mollis sit aliquam sit nullam.
             </p>
@@ -139,6 +137,51 @@ export default function Home() {
               {fakeCounselorAry.map((data, index) => {
                 return <CounselorCard key={index} />
               })}
+            </ul>
+          </div>
+        </section>
+
+        {/* 平台特色 */}
+        <section>
+          <div className='container py-20 text-center lg:flex lg:justify-center lg:space-x-[216px] lg:py-[157px]'>
+            <div>
+              {/* 這裡是圖片 */}
+              <div className='hidden rounded bg-secondary lg:mb-11 lg:block lg:h-[108px] lg:w-[108px]'></div>
+              <h2 className='lg:text-left'>平台特色</h2>
+              <p className='subTitle mb-10 lg:max-w-[326px] lg:text-left'>
+                Lorem ipsum dolor sit amet consectet adipiscing elit eget
+                quamumto.
+              </p>
+            </div>
+
+            <ul className='flex flex-wrap'>
+              <li className='flex w-1/2 flex-col items-center justify-center  border-b border-secondary p-6 text-primary-heavy'>
+                <h3 className='mb-2 text-lg  font-bold lg:text-[28px]'>
+                  客製化
+                </h3>
+                <p className='text-xs lg:text-lg'>Lorem ipsum dolor</p>
+              </li>
+
+              <li className='flex w-1/2 flex-col items-center justify-center  border-b border-l border-secondary p-6 text-primary-heavy'>
+                <h3 className='mb-2 text-lg  font-bold lg:text-[28px]'>
+                  全程保密
+                </h3>
+                <p className='text-xs lg:text-lg'>Lorem ipsum dolor</p>
+              </li>
+
+              <li className='flex w-1/2 flex-col items-center justify-center p-6 text-primary-heavy'>
+                <h3 className='mb-2 text-lg  font-bold lg:text-[28px]'>
+                  體驗課程
+                </h3>
+                <p className='text-xs lg:text-lg'>Lorem ipsum dolor</p>
+              </li>
+
+              <li className='flex w-1/2 flex-col items-center justify-center  border-l border-secondary p-6 text-primary-heavy'>
+                <h3 className='mb-2 text-lg  font-bold lg:text-[28px]'>
+                  專業心理師
+                </h3>
+                <p className='text-xs lg:text-lg'>Lorem ipsum dolor</p>
+              </li>
             </ul>
           </div>
         </section>
