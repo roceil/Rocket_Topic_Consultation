a934c7012aefc3159ad73ca67a6dd31ec3e72058
roceil <a0978006326@gmail.com>
2023-03-07 21:39:30 +0800

    [Feat] finish reservationTour layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,7 +62,7 @@ export default function Home() {
       </Head>
 
       {/* navbar */}
-      <nav className='container my-[18px] flex items-center justify-between xl:container xl:my-[30px]'>
+      {/* <nav className='container my-[18px] flex items-center justify-between xl:container xl:my-[30px]'>
         <div className='text-2xl font-bold leading-normal text-[#5D5A88]'>
           Logo
         </div>
@@ -119,7 +119,7 @@ export default function Home() {
         <div className='flex h-full items-center justify-center'>
           <h1 className='text-6xl font-bold'>諮商平台的環境啦～～～</h1>
         </div>
-      </main>
+      </main> */}
 
       <main>
         {/* 推薦諮商師 */}
@@ -183,6 +183,51 @@ export default function Home() {
                 <p className='text-xs lg:text-lg'>Lorem ipsum dolor</p>
               </li>
             </ul>
+          </div>
+        </section>
+
+        {/* 預約教學 */}
+        <section className='bg-[#FAFAFF]'>
+          <div className='container py-20 text-center lg:py-[180px]'>
+            <div className='lg:flex lg:justify-between '>
+              <div className=''>
+                <h2 className='mb-9 lg:mt-[68px] lg:text-left'>預約教學</h2>
+              </div>
+
+              <ul className='flex flex-col items-center lg:w-[704px] lg:flex-row lg:flex-wrap  lg:items-start lg:justify-between'>
+                <li className='flex w-[280px] flex-col items-center lg:-mb-11 lg:w-[328px]'>
+                  {/* 這裡是圖片 */}
+                  <div className='h-[360px] w-[280px] rounded bg-secondary lg:block lg:h-[415px] lg:w-full'></div>
+                  <p className='mt-1 w-full text-left text-xl font-bold text-primary-heavy lg:mt-3'>
+                    Step1
+                  </p>
+                </li>
+
+                <li className='flex w-[280px] flex-col items-center lg:mt-[182px] lg:mb-11 lg:w-[328px]'>
+                  {/* 這裡是圖片 */}
+                  <div className='h-[360px] w-[280px] rounded bg-secondary lg:block lg:h-[415px] lg:w-full'></div>
+                  <p className='mt-1 w-full text-left text-xl font-bold text-primary-heavy lg:mt-3'>
+                    Step2
+                  </p>
+                </li>
+
+                <li className='flex w-[280px] flex-col items-center lg:!mt-[-182px] lg:w-[328px]'>
+                  {/* 這裡是圖片 */}
+                  <div className='h-[360px] w-[280px] rounded bg-secondary lg:block lg:h-[415px] lg:w-full'></div>
+                  <p className='mt-1 w-full text-left text-xl font-bold text-primary-heavy lg:mt-3'>
+                    Step3
+                  </p>
+                </li>
+
+                <li className='flex w-[280px] flex-col items-center lg:w-[328px] '>
+                  {/* 這裡是圖片 */}
+                  <div className='h-[360px] w-[280px] rounded bg-secondary lg:block lg:h-[415px] lg:w-full'></div>
+                  <p className='mt-1 w-full text-left text-xl font-bold text-primary-heavy lg:mt-3'>
+                    Step4
+                  </p>
+                </li>
+              </ul>
+            </div>
           </div>
         </section>
       </main>
