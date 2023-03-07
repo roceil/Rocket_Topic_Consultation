a04ebb8368b40468adb4e1e2eb66593b1683a7c3
roceil <a0978006326@gmail.com>
2023-03-07 17:47:10 +0800

    [style] adjust counselorCard gap

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,21 +59,24 @@ export default function Home() {
         <section className='bg-[#FAFAFF] '>
           <div className='container flex w-screen flex-col items-center  py-20 text-center'>
             <h2 className='font-bold text-primary-heavy '>推薦諮商師</h2>
-            <p className='mt-2 text-primary lg:mt-3 lg:max-w-[612px] lg:text-lg'>
+            <p className='mt-2 mb-3 text-primary lg:mt-3 lg:max-w-[612px] lg:text-lg lg:mb-[76px]'>
               Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit
               phasellus mollis sit aliquam sit nullam.
             </p>
             <div className='block lg:hidden'>
               <CounselorCard />
             </div>
-            <ul className='hidden lg:flex lg:flex-wrap lg:justify-center'>
+            <ul className='hidden lg:flex lg:flex-wrap lg:justify-center lg:gap-16'>
               {fakeCounselorAry.map((data, index) => {
                 return <CounselorCard key={index} />
               })}
             </ul>
           </div>
         </section>
+        <footer></footer>
       </main>
     </>
   )
 }
+
+
