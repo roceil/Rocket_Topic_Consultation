09b48e4bea297e0805699ba87159a45edb5dc6f6
Sophie <2020soop@gmail.com>
2023-03-12 18:05:24 +0800

    [Feat] Feat Layout (PC) and form (諮商師＆用戶)

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,17 +1,24 @@
+// 待修改：
+// 表單 label padding 還沒改
+// PC 表單格線有問題
+// PC 左側底圖，要重新定位、高度要自適應
+
 import React from 'react';
 import { ConfigProvider, Tabs, Button,
   Checkbox,
   Form,
   Input,
   Select,
-  DatePicker} from 'antd';
+  DatePicker,
+  Upload} from 'antd';
+import { PlusCircleOutlined } from '@ant-design/icons'
 
 const { Option } = Select;
 
 const formItemLayout = {
   labelCol: {
     xs: {
-      span: 24,
+      span: 4,
     },
     sm: {
       span: 8,
@@ -19,10 +26,10 @@ const formItemLayout = {
   },
   wrapperCol: {
     xs: {
-      span: 24,
+      span: 8,
     },
     sm: {
-      span: 16,
+      span: 24,
     },
   },
 };
@@ -33,13 +40,17 @@ const tailFormItemLayout = {
       offset: 0,
     },
     sm: {
-      span: 16,
-      offset: 8,
+      span: 24,
+      offset: 0,
     },
   },
 };
 
 const inputStyle = 'py-3 px-5 rounded-[24px]';
+
+
+
+
 // -----------------------
 
 
@@ -53,173 +64,313 @@ export default function SignIn (){
   const config = {
     rules: [{ type: 'object' as const, required: true, message: 'Please select time!' }],
   };
-
+  const normFile = (e: any) => {
+    console.log('Upload event:', e);
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e?.fileList;
+  };
+  const SignInAry = [
+    {user: '用戶',
+    form: <><Form
+    {...formItemLayout}
+    form={form}
+    name="register"
+    onFinish={onFinish}
+    className='space-y-8 max-w-[380px]'
+  >
+    <Form.Item
+      name="Name"
+      label="姓名 Name"
+      rules={[
+        {
+          required: true,
+          message: '請輸入姓名',
+          whitespace: true,
+        },
+      ]}
+      style={{width: 180,}}
+    >
+      <Input placeholder='Name' className={inputStyle}/>
+    </Form.Item>
+    <Form.Item
+      name="gender"
+      label="性別 Sex"
+      rules={[
+        {
+          required: true,
+          message: '請選擇性別',
+        },
+      ]}
+      style={{width: 180,}}
+    >
+      <Select placeholder="選擇性別" >
+        <Option value="male">男</Option>
+        <Option value="female">女</Option>
+        <Option value="other">其他</Option>
+      </Select>
+    </Form.Item>
+  
+    <Form.Item name="date-picker" label="出生年月日 Birth date" {...config}>
+      <DatePicker className={inputStyle} style={{width: 380}}/>
+    </Form.Item>
+  
+    <Form.Item
+      name="email"
+      label="帳號 Account"
+      rules={[
+        {
+          type: 'email',
+          message: '請輸入 E-mail',
+        },
+        {
+          required: true,
+          message: '請輸入 E-mail',
+        },
+      ]}
+      
+    >
+      <Input placeholder="Email address" className={inputStyle}/>
+    </Form.Item>
+  
+    <Form.Item
+      name="password"
+      label="密碼 Password"
+      rules={[
+        {
+          required: true,
+          message: '請輸入密碼',
+        },
+      ]}
+      hasFeedback
+    >
+      <Input.Password placeholder="Password" className={inputStyle}/>
+      <p className='text-right'>須包含大小寫英文字母及數字</p>
+    </Form.Item>
+  
+    <Form.Item
+      name="confirm"
+      label="再次輸入密碼 Confirm password"
+      dependencies={['password']}
+      hasFeedback
+      rules={[
+        {
+          required: true,
+          message: '請再次輸入密碼',
+        },
+        ({ getFieldValue }) => ({
+          validator(_, value) {
+            if (!value || getFieldValue('password') === value) {
+              return Promise.resolve();
+            }
+            return Promise.reject(new Error('The two passwords that you entered do not match!'));
+          },
+        }),
+      ]}
+    >
+      <Input.Password placeholder='Confirm password' className={inputStyle}/>
+    </Form.Item>
+  
+    <Form.Item
+      name="agreement"
+      valuePropName="checked"
+      rules={[
+        {
+          validator: (_, value) =>
+            value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
+        },
+      ]}
+      {...tailFormItemLayout}
+    >
+      <Checkbox className='mt-[84px]'>
+      我已同意 <a href="">隱私權條款</a>
+      </Checkbox>
+    </Form.Item>
+    <Form.Item {...tailFormItemLayout}>
+      <Button type="default" htmlType="submit" className='bg-[#D4D2E3] text-white h-[56px] w-[380px] rounded-[30px] text-base '>
+      立即註冊
+      </Button>
+    </Form.Item>
+    </Form>,</>},
+    {user: '諮商師',
+    form:<><Form
+    {...formItemLayout}
+    form={form}
+    name="register"
+    onFinish={onFinish}
+    style={{
+      maxWidth: 380,
+    }}
+    className='space-y-8'
+  >
+    <Form.Item
+      name="Name"
+      label="諮商師姓名 Name"
+      rules={[
+        {
+          required: true,
+          message: '請輸入姓名',
+          whitespace: true,
+        },
+      ]}
+      style={{width: 180,}}
+    >
+      <Input placeholder='Name' className={inputStyle}/>
+    </Form.Item>
+    <Form.Item
+      name="upload"
+      label="諮商師執照 License"
+      valuePropName="fileList"
+      getValueFromEvent={normFile}
+      
+    >
+      <Upload name="logo" action="/upload.do" listType="picture">
+        <Button icon={<PlusCircleOutlined />} >License</Button>
+      </Upload>
+    </Form.Item>
+    <Form.Item
+      name="Name"
+      label="諮商師證書字號 Certification No."
+      rules={[
+        {
+          required: true,
+          message: '請輸入諮商師證書字號 Certification No.',
+          whitespace: true,
+        },
+      ]}
+    >
+      <Input placeholder='Certification No.' className={inputStyle}/>
+    </Form.Item>
+  
+    <Form.Item
+      name="email"
+      label="帳號 Account"
+      rules={[
+        {
+          type: 'email',
+          message: '請輸入 E-mail',
+        },
+        {
+          required: true,
+          message: '請輸入 E-mail',
+        },
+      ]}
+      
+    >
+      <Input placeholder="Email address" className={inputStyle}/>
+    </Form.Item>
+  
+    <Form.Item
+      name="password"
+      label="密碼 Password"
+      rules={[
+        {
+          required: true,
+          message: '請輸入密碼',
+        },
+      ]}
+      hasFeedback
+    >
+      <Input.Password placeholder="Password" className={inputStyle}/>
+      <p className='text-right'>須包含大小寫英文字母及數字</p>
+    </Form.Item>
+  
+    <Form.Item
+      name="confirm"
+      label="再次輸入密碼 Confirm password"
+      dependencies={['password']}
+      hasFeedback
+      rules={[
+        {
+          required: true,
+          message: '請再次輸入密碼',
+        },
+        ({ getFieldValue }) => ({
+          validator(_, value) {
+            if (!value || getFieldValue('password') === value) {
+              return Promise.resolve();
+            }
+            return Promise.reject(new Error('The two passwords that you entered do not match!'));
+          },
+        }),
+      ]}
+    >
+      <Input.Password placeholder='Confirm password' className={inputStyle}/>
+    </Form.Item>
+  
+    <Form.Item
+      name="agreement"
+      valuePropName="checked"
+      rules={[
+        {
+          validator: (_, value) =>
+            value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
+        },
+      ]}
+      {...tailFormItemLayout}
+    >
+      <Checkbox className='mt-[84px]'>
+      我已同意 <a href="">隱私權條款</a>
+      </Checkbox>
+    </Form.Item>
+    <Form.Item {...tailFormItemLayout}>
+      <Button type="default" htmlType="submit" className='bg-[#D4D2E3] text-white h-[56px] w-[380px] rounded-[30px] text-base '>
+      立即註冊
+      </Button>
+    </Form.Item>
+    </Form></>}
+  ]
 
   return(
     <>
-    <div className=" container w-screen flex justify-center my-12">
-      <h2>會員註冊</h2>
-    </div>
-    <div className="container">
-      <ConfigProvider
-        theme={{
-          token: {
-            colorText: '#5D5A88', // Tab 文字顏色
-            colorPrimary: '#5D5A88', // Tab 被選取時的顏色 
-            borderRadius: 100,
-            colorBgContainer:'#EEECFA',
-            colorBorder: '#EEECFA',
-            colorIcon: '#5D5A88',
-            colorTextDescription: '#ff7f17'
-          },
-          components:{
-            Checkbox:{
-              colorBorder: '#EEECFA',
-              borderRadius: 4
-            }
-          }
-        }}
-      >
-        <Tabs
-          defaultActiveKey="1"
-          centered
-          items={new Array(2).fill(null).map((_, i) => {
-            const id = String(i + 1);
-            const uesrAry = ['用戶', '諮商師']
-
-            return {
-              label: `我是${uesrAry}`,
-              key: id,
-              children: `Content of Tab Pane ${id}`,
-            };
-          })}
-        />
-
-    <Form
-      {...formItemLayout}
-      form={form}
-      name="register"
-      onFinish={onFinish}
-      style={{
-        maxWidth: 380,
-      }}
-    >
-      <Form.Item
-        name="Name"
-        label="姓名 Name"
-        rules={[
-          {
-            required: true,
-            message: '請輸入姓名',
-            whitespace: true,
-          },
-        ]}
-        style={{width: 180,}}
-      >
-        <Input placeholder='Name' className={inputStyle}/>
-      </Form.Item>
-      <Form.Item
-        name="gender"
-        label="性別 Sex"
-        rules={[
-          {
-            required: true,
-            message: '請選擇性別',
-          },
-        ]}
-        style={{width: 180,}}
-      >
-        <Select placeholder="選擇性別" >
-          <Option value="male">男</Option>
-          <Option value="female">女</Option>
-          <Option value="other">其他</Option>
-        </Select>
-      </Form.Item>
-
-      <Form.Item name="date-picker" label="出生年月日 Birth date" {...config}>
-        <DatePicker className={inputStyle} style={{width: 380,}}/>
-      </Form.Item>
-
-      <Form.Item
-        name="email"
-        label="帳號 Account"
-        rules={[
-          {
-            type: 'email',
-            message: 'The input is not valid E-mail!',
-          },
-          {
-            required: true,
-            message: 'Please input your E-mail!',
-          },
-        ]}
-        
-      >
-        <Input placeholder="Email address" className={inputStyle}/>
-      </Form.Item>
-
-      <Form.Item
-        name="password"
-        label="密碼 Password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password placeholder="Password" className={inputStyle}/>
-        <p className='text-right'>須包含大小寫英文字母及數字</p>
-      </Form.Item>
-
-      <Form.Item
-        name="confirm"
-        label="再次輸入密碼 Confirm password"
-        dependencies={['password']}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: 'Please confirm your password!',
-          },
-          ({ getFieldValue }) => ({
-            validator(_, value) {
-              if (!value || getFieldValue('password') === value) {
-                return Promise.resolve();
+    <div className='hidden lg:block w-[416px] h-[1220px] bg-[#D4D2E3] absolute -z-[99]'></div>
+    {/* PC 左側底圖 */}
+    <div className="z-10 container flex">
+      {/* 圖片 */}
+      <div className='hidden lg:block w-[492px] h-[492px] rounded-[25px] bg-primary-light mr-[176px] mt-[120px] ml-[58px] text-center'>Image</div>
+      {/* 右側輸入區 */}
+      <div className="max-w-[380px] lg:mt-[120px] lg:mb-[160px] mb-[84px]">
+        <div className='flex flex-col items-center my-12 lg:mt-0 lg:mb-12 lg:items-start'>
+          <p className='text-sm text-primary-heavy font-bold mb-1 hidden lg:block'>SIGN UP</p>
+          <h2>會員註冊</h2>
+        </div>
+        <div className='max-w-[380px]'>
+          <ConfigProvider
+            theme={{
+              token: {
+                colorTextPlaceholder: '#5D5A88',
+                colorPrimary: '#5D5A88', // Tab 被選取時的顏色 
+                borderRadius: 24,
+                colorText: '#5D5A88',
+                colorIcon: '#5D5A88',
+                colorTextDescription: '#ff7f17',
+                colorBorder: '#D4D2E3'
+              },
+              components:{
+                Tabs:{
+                  colorText: '#D4D2E3',
+                  borderRadius: 4
+                },
               }
-              return Promise.reject(new Error('The two passwords that you entered do not match!'));
-            },
-          }),
-        ]}
-      >
-        <Input.Password placeholder='Confirm password' className={inputStyle}/>
-      </Form.Item>
-
-      <Form.Item
-        name="agreement"
-        valuePropName="checked"
-        rules={[
-          {
-            validator: (_, value) =>
-              value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
-          },
-        ]}
-        {...tailFormItemLayout}
-      >
-        <Checkbox>
-        我已同意 <a href="">隱私權條款</a>
-        </Checkbox>
-      </Form.Item>
-      <Form.Item {...tailFormItemLayout}>
-        <Button type="default" htmlType="submit" className='bg-primary-heavy text-white h-[56px] w-[380px] rounded-[30px] text-base '>
-        立即註冊
-        </Button>
-      </Form.Item>
-    </Form>
-
-
-      </ConfigProvider>
+            }}
+          >
+            <Tabs
+              defaultActiveKey="1"
+              centered
+              items={SignInAry.map((_, i) => {
+                const id = String(i + 1);
+                return {
+                  label: `我是${_.user}`,
+                  key: id,
+                  children: _.form
+                  
+                };
+              })}
+            />
+          </ConfigProvider>
+        </div>
+      </div>
     </div>
     </>
   )
