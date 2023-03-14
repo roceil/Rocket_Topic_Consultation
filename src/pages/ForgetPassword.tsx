7799d9013bb9bfa06dce168e976a760b7f08f3ae
Sophie <2020soop@gmail.com>
2023-03-14 12:08:50 +0800

    [Feat] reset form margin and padding (PC & Mobile)

--- src/pages/ForgetPassword.tsx
+++ src/pages/ForgetPassword.tsx
@@ -1,19 +1,12 @@
 // 待修改：
-// PC 表單 文字＆輸入框 要換行排列-> <Form layout="ant-col"> 改得動但有紅字，估計 vercel 會報錯
 // PC 左側底圖高度 -> 無法自適應，目前自訂 css 算出 div 高度，試過 Footer 給 z-10，底圖給 -z-[99] 無效
 
 import Link from 'next/link';
 import React from 'react';
-import { ConfigProvider, Tabs, Button,
+import { ConfigProvider, Button,
   Checkbox,
   Form,
-  Input,
-  Select,
-  Upload} from 'antd';
-import { PlusCircleOutlined } from '@ant-design/icons'
-import Column from 'antd/es/table/Column';
-
-const { Option } = Select;
+  Input} from 'antd';
 
 const formItemLayout = {
   labelCol: {
@@ -23,15 +16,7 @@ const formItemLayout = {
     sm: {
       span: 24,
     },
-  },
-  wrapperCol: {
-    xs: {
-      span: 0,
-    },
-    sm: {
-      span: 0,
-    },
-  },
+  }
 };
 const tailFormItemLayout = {
   wrapperCol: {
@@ -48,100 +33,12 @@ const tailFormItemLayout = {
 
 const inputStyle = 'py-3 px-5 rounded-[24px] ';
 
-
-
-
-// -----------------------
-
-
-
 export default function LogIn (){
 
   const [form] = Form.useForm();
   const onFinish = (values:any) => {
     console.log('Received values of form: ', values);
   };
-  const SignInAry = [
-    {user: '用戶',
-    form: <><Form
-    {...formItemLayout}
-    form={form}
-    name="register"
-    onFinish={onFinish}
-    className='space-y-8 max-w-[380px]'
-    layout="ant-col"
-    labelAlign="left"
-  >
-    <Form.Item
-      name="email"
-      label="帳號 Account"
-    >
-      <Input placeholder="Email address" className={inputStyle}/>
-    </Form.Item>
-  
-    <Form.Item
-      name="password"
-      label="密碼 Password"
-      hasFeedback
-    >
-      <Input.Password placeholder="Password" className={inputStyle}/>
-      <p className='text-right underline'>忘記密碼？</p>
-    </Form.Item>
-    {/* 推底下間距 */}
-    <div className='h-5'></div> 
-    <div className='flex justify-between items-end'>
-      <Form.Item className='flex items-end m-0'>
-        <Checkbox>
-        記住我
-        </Checkbox>
-      </Form.Item>
-      <div className='flex h-8 items-center'>
-        <p>尚未成為會員？</p>
-        <Link href ='#'>
-          <p className='underline ml-2'>立即註冊</p>
-        </Link>
-      </div>
-    </div>
-    <Form.Item {...tailFormItemLayout}>
-      <Button type="default" htmlType="submit" className='-mt-8 bg-[#D4D2E3] text-white h-[56px] w-[380px] rounded-[30px] text-base '>
-      登入
-      </Button>
-    </Form.Item>
-    </Form></>},
-    {user: '諮商師',
-    form:<><Form
-    {...formItemLayout}
-    form={form}
-    name="register"
-    onFinish={onFinish}
-    className='space-y-8 max-w-[380px]'
-    layout="ant-col"
-    labelAlign="left"
-  >
-    <Form.Item
-      name="email"
-      label="帳號 Account"
-    >
-      <Input placeholder="Email address" className={inputStyle}/>
-      <p className='text-right'>請輸入註冊時使用的信箱</p>
-    </Form.Item>
-    {/* 推底下間距 */}
-    <div className='h-5'></div> 
-    <div className='flex justify-between items-end'>
-      <div className='flex h-8 items-center'>
-        <p>未收到信件？</p>
-        <Link href ='#'>
-          <p className='underline ml-2'>重新發送</p>
-        </Link>
-      </div>
-    </div>
-    <Form.Item {...tailFormItemLayout}>
-      <Button type="default" htmlType="submit" className='-mt-8 bg-[#D4D2E3] text-white h-[56px] w-[380px] rounded-[30px] text-base '>
-      登入
-      </Button>
-    </Form.Item>
-    </Form></>}
-  ]
 
   return(
     <>
@@ -156,28 +53,20 @@ export default function LogIn (){
           <p className='text-base text-primary-heavy font-bold mb-1 hidden lg:block'>FORGET PASSWORD</p>
           <h2>忘記密碼</h2>
         </div>
-        <div className='max-w-[380px] flex fle-col '>
-          <ConfigProvider
+        <div className='max-w-[380px] flex fle-col form'>
+        <ConfigProvider
             theme={{
               token: {
                 colorTextPlaceholder: '#5D5A88',
-                colorPrimary: '#5D5A88', // Tab 被選取時的顏色 
-                // borderRadius: 24,
                 colorText: '#5D5A88',
-                colorIcon: '#5D5A88',
-                colorTextDescription: '#ff7f17',
                 colorBorder: '#D4D2E3'
               },
               components:{
-                Tabs:{
-                  colorText: '#D4D2E3',
-                  borderRadius: 4
-                },
-                Select:{
-                  controlHeight: 48
-                },
-                Checkbox:{
-                  borderRadius: 0,
+                Button:{
+                  colorPrimaryHover: '#5D5A88',
+                  colorPrimaryActive: '#5D5A88',
+                  colorText: '#5D5A88',
+                  colorTextDisabled: '#fff'
                 }
               }
             }}
@@ -188,18 +77,17 @@ export default function LogIn (){
               name="register"
               onFinish={onFinish}
               className='space-y-8 max-w-[380px]'
-              layout="ant-col"
               labelAlign="left"
             >
               <Form.Item
                 name="email"
                 label="信箱 Email Address"
               >
-                <Input placeholder="Email address" className={inputStyle}/>
-                <p className='text-right'>請輸入註冊時使用的信箱</p>
+                <Input placeholder="Email address" className={`mt-11 ${inputStyle}`}/>
+                <p className='text-right py-[1px]'>請輸入註冊時使用的信箱</p>
               </Form.Item>
               {/* 推底下間距 */}
-              <div className='h-5'></div> 
+              <div className='h-5 lg:h-[104px]'></div> 
               <div className='flex justify-between items-end'>
                 <div className='flex h-8 items-center'>
                   <p>未收到信件？</p>
@@ -209,8 +97,8 @@ export default function LogIn (){
                 </div>
               </div>
               <Form.Item {...tailFormItemLayout}>
-                <Button type="default" htmlType="submit" className='-mt-8 bg-[#D4D2E3] text-white h-[56px] w-[380px] rounded-[30px] text-base '>
-                登入
+              <Button type="primary" shape='round' htmlType="submit" className='-mt-8 bg-[#D4D2E3] text-white h-[56px] w-[380px] text-base shadow-none'>
+                發送密碼重設信
                 </Button>
               </Form.Item>
               </Form>
