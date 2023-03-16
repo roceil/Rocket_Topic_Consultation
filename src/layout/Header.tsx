16eb2ecb99a93dd88fc900c6b1e15e6dd2dc5b59
Sophie <2020soop@gmail.com>
2023-03-16 16:29:26 +0800

    [Feat] feat HamburgerMenu and fix ESlint problems

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,91 +1,174 @@
-import Link from 'next/link'
-import { useDispatch } from 'react-redux'
-import { userCenterPosition } from '@/redux/feature/userCenter'
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable react/button-has-type */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable import/extensions */
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import {
   ShoppingCartOutlined,
   BellOutlined,
-  UserOutlined
-} from '@ant-design/icons'
-import { Button, ConfigProvider, Input } from 'antd'
-import { IButton, darkBtn, lightBtn } from '../components/Public/IButton'
+  UserOutlined,
+  SearchOutlined,
+  MenuOutlined,
+} from '@ant-design/icons';
+import {
+  Button, ConfigProvider, Input, Modal, Space,
+} from 'antd';
+import { userCenterPosition } from '@/redux/feature/userCenter';
+import { IButton, darkBtn, lightBtn } from '../components/Public/IButton';
 
-const isLogIn = false // 判斷是否登入，控制 Nav 顯示內容
+const isLogIn = false; // 判斷是否登入，控制 Nav 顯示內容
 
 // 登入時，顯示『會員中心、通知』icons
 const LogInIcons = (
   <>
     <Button
-      type='default'
-      shape='circle'
-      size='large'
-      icon={
+      type="default"
+      shape="circle"
+      size="large"
+      icon={(
         <BellOutlined
           style={{
             fontSize: '20px',
-            color: '#8D8BA7'
+            color: '#8D8BA7',
           }}
         />
-      }
+      )}
     />
     <Button
-      type='default'
-      shape='circle'
-      size='large'
-      icon={
+      type="default"
+      shape="circle"
+      size="large"
+      icon={(
         <UserOutlined
           style={{
             fontSize: '20px',
-            color: '#8D8BA7'
+            color: '#8D8BA7',
           }}
         />
-      }
+      )}
     />
   </>
-)
+);
 
 // 未登入時，顯示『登入/註冊』Btn
 const YetLogInBtn = (
   <IButton
-    text='登入 / 註冊'
+    text="登入 / 註冊"
     bgColor={lightBtn}
-    fontSize='text-[14px] lg:text-base'
-    px='px-6'
-    py='py-3'
-  ></IButton>
-)
+    fontSize="text-[14px] lg:text-base"
+    px="px-6"
+    py="py-3"
+  />
+);
 
 // Header - input 元件(PC)
-const { Search } = Input
-export const onSearch = (value: any) => console.log(value)
+const { Search } = Input;
+export const onSearch = (value: any) => console.log(value);
 
 export function Header() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+
+  // 漢堡選單開關
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <header className='my-[18px] lg:my-0 lg:py-[30px]'>
-      <div className='container flex items-center justify-between'>
+    <header className="my-[18px] lg:my-0 lg:py-[30px]">
+      <div className="container flex items-center justify-between">
         <Link
-          href={'UserCenter'}
-          className='cursor-pointer text-2xl font-bold leading-normal text-[#5D5A88] hover:opacity-50'
+          href="UserCenter"
+          className="cursor-pointer text-2xl font-bold leading-normal text-[#5D5A88] hover:opacity-50"
           onClick={() => {
-            dispatch(userCenterPosition('個人資料'))
+            dispatch(userCenterPosition('個人資料'));
           }}
         >
           Logo
         </Link>
+        {/* 漢堡選單 */}
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimary: '#5D5A88',
+              colorLink: '#5D5A88',
+              colorLinkActive: '#8D8BA7',
+              colorLinkHover: '#8D8BA7',
+              colorIcon: '#5D5A88',
+            },
+            components: {},
+          }}
+        >
+          <Button onClick={showModal} icon={<MenuOutlined />} type="primary" shape="round" className="text-primary-heavy text-base shadow-none lg:hidden" />
+          <Modal
+            width={348}
+            open={isModalOpen}
+            onCancel={handleCancel}
+            footer={null}
+            maskStyle={{
+              backgroundColor: '#EEECFA', boxShadow: 'none', display: 'flex', justifyContent: 'center',
+            }}
+            className="hamburger-menu"
+          >
+            {/* 課程連結 */}
+            <div className="bg-[#EEECFA] w-[276px] flex flex-col justify-center rounded-[10px] mt-12 mb-[52px]">
+              <p className="text-center text-base text-primary-heavy my-3 font-bold">目前尚無預約</p>
+            </div>
+            <Space direction="vertical" style={{ width: 276 }}>
+              <Link href="/CounselorList">
+                <Button type="link" onClick={handleCancel} icon={<SearchOutlined />} className="font-bold text-lg">
+                  諮商師總覽
+                </Button>
+              </Link>
+              <div className="border border-[#D4D2E3] border-t-[1px] my-2" />
+              <Link href="/ShopCart">
+                <Button type="link" icon={<ShoppingCartOutlined />} onClick={handleCancel} className="font-bold text-lg">
+                  購物車
+                </Button>
+              </Link>
+              <div className="border border-[#D4D2E3] border-t-[1px] my-2" />
+              <Button type="link" icon={<UserOutlined />} className="font-bold text-lg">
+                會員中心
+              </Button>
+              <Button type="link" className="text-base ml-[26px]">
+                個人資料
+              </Button>
+              <Button type="link" className="text-base ml-[26px]">
+                預約管理
+              </Button>
+              <Button type="link" className="text-base ml-[26px]">
+                個案記錄
+              </Button>
+              <div className="border border-[#D4D2E3] border-t-[1px] my-2" />
+            </Space>
 
-        <div className='flex h-6 w-6 items-center justify-center  lg:hidden xl:hidden'>
-          <Link href={'UserCenter'}>
+            <Button type="primary" shape="round" htmlType="submit" className="bg-[#5D5A88] text-white h-[56px] w-[276px] text-base shadow-none mt-20">
+              登出
+            </Button>
+          </Modal>
+        </ConfigProvider>
+
+        <div className="flex h-6 w-6 items-center justify-center  lg:hidden xl:hidden">
+          <Link href="UserCenter">
             <button
-              className='h-[10px] w-[18px] border-y-2 border-y-[#5D5A88]'
+              className="h-[10px] w-[18px] border-y-2 border-y-[#5D5A88]"
               onClick={() => {
-                dispatch(userCenterPosition('預約管理'))
+                dispatch(userCenterPosition('預約管理'));
               }}
-            ></button>
+            />
           </Link>
         </div>
         {/* PC 版導覽列 */}
-        <div className='hidden lg:block'>
-          <ul className='flex items-center space-x-5'>
+        <div className="hidden lg:block">
+          <ul className="flex items-center space-x-5">
             <ConfigProvider
               theme={{
                 token: {
@@ -93,45 +176,45 @@ export function Header() {
                   borderRadiusLG: 100,
                   borderRadius: 100,
                   colorBorder: '#D4D2E3',
-                  colorFillAlter: '#FFF'
+                  colorFillAlter: '#FFF',
                 },
-                components: {}
+                components: {},
               }}
             >
               <Search
-                placeholder=''
+                placeholder=""
                 onSearch={onSearch}
-                size='large'
+                size="large"
                 style={{
-                  width: 180
+                  width: 180,
                 }}
               />
               <Button
-                type='default'
-                shape='circle'
-                size='large'
-                icon={
+                type="default"
+                shape="circle"
+                size="large"
+                icon={(
                   <ShoppingCartOutlined
                     style={{
                       fontSize: '20px',
-                      color: '#8D8BA7'
+                      color: '#8D8BA7',
                     }}
                   />
-                }
+                )}
               />
               {isLogIn && LogInIcons}
             </ConfigProvider>
             {!isLogIn && YetLogInBtn}
             <IButton
-              text='尋找諮商師'
+              text="尋找諮商師"
               bgColor={darkBtn}
-              fontSize='text-[14px] lg:text-base'
-              px='px-6'
-              py='py-3'
-            ></IButton>
+              fontSize="text-[14px] lg:text-base"
+              px="px-6"
+              py="py-3"
+            />
           </ul>
         </div>
       </div>
     </header>
-  )
+  );
 }
