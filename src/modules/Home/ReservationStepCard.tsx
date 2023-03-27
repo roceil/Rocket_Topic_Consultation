07adccf132d724228dd133c72d60906f78b0f9fd
roceil <a0978006326@gmail.com>
2023-03-27 19:48:14 +0800

    [Fix] try to fix reservationTour pic unwork problems

--- src/modules/Home/ReservationStepCard.tsx
+++ src/modules/Home/ReservationStepCard.tsx
@@ -2,13 +2,14 @@ import Image from 'next/image';
 import { IReservationStepCardProps } from '@/types/interface';
 
 export default function ReservationStepCard({ step, img, extraStyle }: IReservationStepCardProps) {
+  const test = img;
   return (
     <li className={`${extraStyle} flex flex-col items-center`}>
       {/* 文字區塊 */}
       <p className="mb-2 w-full text-left text-xl font-bold text-secondary lg:mt-3">{step}</p>
 
       {/* 圖片區塊 */}
-      <Image src={img} alt="stepPicture" className="h-[360px] w-[280px] lg:block lg:h-[415px] lg:w-[328px]" />
+      <Image src={test} alt="stepPicture" width={280} height={360} className="h-[360px] w-[280px] lg:block lg:h-[415px] lg:w-[328px]" placeholder="empty" />
     </li>
   );
 }
